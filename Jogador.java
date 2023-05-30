1bbacff2b00e0f3330fb969dda2f735675d6c00a
Hyn4 <yhanpena@hotmail.com>
2023-05-30 17:18:09 -0300

    UpdateCombatManegerComeco

--- Jogador.java
+++ Jogador.java
@@ -1,3 +1,29 @@
-public class Jogador{   
+public class Jogador extends Personagem{   
+    private int progresso; //aumenta em 1 a cada ponto de progresso, controla qual inimigo pode lutar
+    private int experiencia; // mede o progresso ate subir de nivel
+    private int nivel; //muda a forca e defesa base do jogador
+
     
+    public Turno<String,Integer> turnoNoCombate(){
+        
+    }
+    
+    public Turno<String,String> turnoNoCombate(){
+        
+    }
+    
+
+    public void receberExperiencia(int experienca){
+        //mensagem: "jogador" recebeu x pontos de experiencia!
+        this.experiencia += experienca;
+        subirDeNivel();
+    }
+
+    private void subirDeNivel(){
+
+    }
+
+    public class Turno<T,V>{
+
+    }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,5 +1,41 @@
 public class Personagem {
     private String nome;
     private String tag;
+    public boolean vivo;
     private int vidaMaxima;
+    private int vidaAtual;
+    private int ataqueBase;
+    private int defesaBase;
+    private int velocidade;
+    private float multiplicadorAtaque;
+    private float multiplicadorDefesa;
+    public String getNome() {
+        return nome;
+    }
+    public String getTag() {
+        return tag;
+    }
+    public int getVidaMaxima() {
+        return vidaMaxima;
+    }
+    public int getVidaAtual() {
+        return vidaAtual;
+    }
+    public int getAtaqueBase() {
+        return ataqueBase;
+    }
+    public int getDefesaBase() {
+        return defesaBase;
+    }
+    public int getVelocidade() {
+        return velocidade;
+    }
+    public boolean estaVivo(){
+        return vivo;
+    }
+
+    // public int turnoNoCombate(){
+
+    // }
+
 }
