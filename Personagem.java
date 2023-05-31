65af3ddd64df6817b83e27a6b931b022f6559305
Hyn4 <yhanpena@hotmail.com>
2023-05-31 18:10:07 -0300

    Comeco do combate e funcoes basicas do jogador

--- Personagem.java
+++ Personagem.java
@@ -1,14 +1,19 @@
 public class Personagem {
     private String nome;
     private String tag;
+    
     public boolean vivo;
+
     private int vidaMaxima;
     private int vidaAtual;
-    private int ataqueBase;
+    private int ataqueBase = 1;
     private int defesaBase;
     private int velocidade;
+
     private float multiplicadorAtaque;
     private float multiplicadorDefesa;
+
+
     public String getNome() {
         return nome;
     }
@@ -17,6 +22,27 @@ public class Personagem {
     }
     public int getVidaMaxima() {
         return vidaMaxima;
+    }
+    public float getMultiplicadorAtaque() {
+        return multiplicadorAtaque;
+    }
+    public void setMultiplicadorAtaque(float multiplicadorAtaque) {
+        this.multiplicadorAtaque = multiplicadorAtaque;
+    }
+    public float getMultiplicadorDefesa() {
+        return multiplicadorDefesa;
+    }
+    public void setMultiplicadorDefesa(float multiplicadorDefesa) {
+        this.multiplicadorDefesa = multiplicadorDefesa;
+    }
+    public void setVidaMaxima(int vidaMaxima) {
+        this.vidaMaxima = vidaMaxima;
+    }
+    public void setAtaqueBase(int ataqueBase) {
+        this.ataqueBase = ataqueBase;
+    }
+    public void setDefesaBase(int defesaBase) {
+        this.defesaBase = defesaBase;
     }
     public int getVidaAtual() {
         return vidaAtual;
@@ -34,8 +60,5 @@ public class Personagem {
         return vivo;
     }
 
-    // public int turnoNoCombate(){
-
-    // }
 
 }
