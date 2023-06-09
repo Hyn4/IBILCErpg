449c4b7115f3b8a8e0c8d96c2c6afeb2ff9672db
MateusRosolem <mateus.rosolem@unesp.br>
2023-06-09 10:04:27 -0300

    css e mais

--- MAIN.java
+++ MAIN.java
@@ -6,18 +6,21 @@ import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 
 public class MAIN extends Application {
 
     @Override
     public void start(@NotNull Stage stage) throws IOException {
-        Font.loadFont(getClass().getResourceAsStream("/8-bit-hud.ttf"), 78);
+        Font.loadFont(getClass().getResourceAsStream("/resources/8-bit-hud.ttf"), 78);
+
+
 
 
         FXMLLoader fxmlLoader = new FXMLLoader(MAIN.class.getResource("Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 930, 540);
-        scene.getStylesheets().addAll(Objects.requireNonNull(this.getClass().getResource("style.css")).toExternalForm());
+        scene.getStylesheets().addAll(Objects.requireNonNull(this.getClass().getResource("stylebutton.css")).toExternalForm());
         stage.setTitle("IbilceRPG");
         stage.setResizable(false);
         stage.setScene(scene);
--- resources/FXController.java
+++ resources/FXController.java
@@ -5,12 +5,26 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class FXController {
-    public Button button_start;
+    @FXML
+    public Button jogarButton;
+    @FXML
+    public Button creditosButton;
+    @FXML
+    public Button sairButton;
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void jogarButtonClick() {
         welcomeText.setText("Bem vindo ao IBILCE RPG!Boa Diversao");
     }
-}
+
+    @FXML
+    protected void creditosButtonClick() {
+        welcomeText.setText("feito pelos melhores");
+    }
+
+    @FXML
+    protected void sairButtonClick(){
+        System.exit(0);
+    }}
