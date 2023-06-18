d7d7c38fab847548afdda0524615e10579b25a71
Hyn4 <yhanpena@hotmail.com>
2023-06-18 20:23:22 -0300

    missoesss

---
+++ Alejandro.java
@@ -0,0 +1,4 @@
+public class Alejandro extends Inimigo {
+    private Missao<ErvasMedicinais> primeiraMissao = new Missao<ErvasMedicinais>("BIOLOGO", new ErvasMedicinais(), "Missão do Arboreto", 3);
+    
+}
